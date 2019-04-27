3d506e7dd7e24ea3b6af356cd18b029ad772b587
sikozonpc <sikozonpc@gmail.com>
2019-04-27 12:49:49 +0100

    nav started

--- js/app.js
+++ js/app.js
@@ -9,7 +9,14 @@ function imageSlide() {
 		currImg++;
 		imgSlider.style.backgroundImage = `url('../assets/imgs/house-${currImg}.jpg')`;
 	}
-	
 }
 
-window.setInterval( () => imageSlide(), 5000);
+window.setInterval(() => imageSlide(), 5000);
+
+// Selecting the backdrop
+let backdrop = document.querySelector(".nav__backgroundclose");
+let nav = document.querySelector(".nav__checkbox");
+backdrop.addEventListener("click", () => {
+	backdrop.style.opacity = 0;
+	nav.checked = false;
+});
